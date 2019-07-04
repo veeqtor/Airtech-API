c27cc576ba1565558a33729e3636ffa6e885cfd7
Veeqtor <nwokeochavictor@gmail.com>
2019-07-04 12:33:53 +0100

    ch(deploy): Deploy to heroku
    -Deploy to heroku
    
    [Maintains #166977630]

--- src/settings/__init__.py
+++ src/settings/__init__.py
@@ -61,6 +61,7 @@ AUTH_USER_MODEL = 'user.user'
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -140,8 +141,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '../src/static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, '../templates/static')]
+STATIC_ROOT = os.path.join(BASE_DIR, '../staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, '../templates/images')]
 
 # Media Folder Settings
 MEDIA_ROOT = os.path.join(BASE_DIR, '../media')
--- src/settings/production.py
+++ src/settings/production.py
@@ -9,6 +9,22 @@ HOST_ENV = environ.get('HOST_ENV')
 
 if HOST_ENV in ENV:
 
+    #  Add configuration for static files storage using whitenoise
+    STATICFILES_STORAGE = 'whitenoise.storage.' \
+                          'CompressedManifestStaticFilesStorage'
+
     # SECURITY WARNING: don't run with debug turned on in production!
     DEBUG = False
-    SSL_ENABLED = False
+    SSL_ENABLED = True
+
+    CACHES = {
+        "default": {
+            "BACKEND": "redis_cache.RedisCache",
+            "LOCATION": os.environ.get('REDIS_URL'),
+        }
+    }
+
+    import django_heroku
+
+    # Activate Django-Heroku.
+    django_heroku.settings(locals())
